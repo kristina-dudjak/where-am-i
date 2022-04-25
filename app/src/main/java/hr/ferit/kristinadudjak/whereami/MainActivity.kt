8a51d483ab41b8460b788ec314484b4f9d664eee
kristina <dudjakkristina@gmail.com>
2022-04-25 09:58:47 +0200

    Add screenshot ability

--- app/src/main/java/hr/ferit/kristinadudjak/whereami/MainActivity.kt
+++ app/src/main/java/hr/ferit/kristinadudjak/whereami/MainActivity.kt
@@ -2,10 +2,19 @@ package hr.ferit.kristinadudjak.whereami
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.location.Geocoder
 import android.media.SoundPool
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -17,6 +26,9 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import hr.ferit.kristinadudjak.whereami.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -32,7 +44,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.bTakePhoto.setOnClickListener{
+            val bitmap = getScreenShotFromView(binding.cardView)
+            if (bitmap != null) {
+                saveMediaToStorage(bitmap)
+            }
+        }
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         this.loadSounds()
@@ -83,6 +105,43 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun saveMediaToStorage(bitmap: Bitmap) {
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            this.contentResolver?.also { resolver ->
+
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, fileName)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, fileName)
+            fos = FileOutputStream(image)
+        }
+        fos?.use {
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this , "Saved to Gallery" , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getScreenShotFromView(v: View): Bitmap? {
+        var screenshot: Bitmap? = null
+        try {
+            screenshot = Bitmap.createBitmap(v.measuredWidth, v.measuredHeight, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(screenshot)
+            v.draw(canvas)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return screenshot
+    }
+
     private fun loadSounds() {
         this.soundPool = SoundPool.Builder().setMaxStreams(10).build()
         this.soundMap[R.raw.click] = this.soundPool.load(this, R.raw.click, 1)
@@ -114,5 +173,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
 }
